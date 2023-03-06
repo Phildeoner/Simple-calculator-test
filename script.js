bc3d65dc5dd9bb215f287b2385a11737c75dd0e4
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-03-06 20:04:57 +0100

    Update performOpeartion function

--- script.js
+++ script.js
@@ -21,14 +21,35 @@ checkOperator();
 // 4. Create a function to perform our calcutions based on the operator selected by the user
 function performOperation() {
 //   a. Ask user for first number or input
-  let firstInput = prompt('Please insert your first number: ');
+  let firstInput = parseInt(prompt('Please insert your first number: '));
 
   //   b. Ask user for second number or input
-  let secondInput = prompt('Please insert your second number: ');
+  let secondInput = parseInt(prompt('Please insert your second number: '));
+
+  //   c. Perform the operation selected on the first and second numbers inputed
+
+  if (operator === 'a') {
+    let addition = firstInput + secondInput
+    console.log(`The sum is ${addition}`);
+    return addition;
+  } else if (operator === 'b') {
+    let subtraction = firstInput - secondInput
+    console.log(`The sum is ${subtraction}`);
+    return subtraction;
+  } else if (operator === 'c') {
+    let division = firstInput / secondInput
+    console.log(`The sum is ${division}`);
+    return division;
+  } else if (operator === 'd') {
+    let multiplication = firstInput * secondInput
+    console.log(`The sum is ${multiplication}`);
+    return multiplication;
+  } else {
+    console.log('Please insert a valid number or input!');
+  }
+  performOperation();
 }
 
-
-//   c. Perform the operation selected on the first and second numbers inputed
 // 5. Create a function to reset the input of opeartor selection and restart the process when there is a wrong selection of opeartor
 function resetOperatorSelection() {
   operator = prompt('Select an operator, a. Addition, b. Subtraction, c. Division, d. Multiplication: ');
